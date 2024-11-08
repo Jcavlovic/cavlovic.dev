4c77fd4180255a3aa29376318cc02c8df858cfdb
Cavlovic <jcavlovic91@gmail.com>
2024-11-07 21:35:40 -0600

    fixed css

--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import NavBar from "./containers/NavBar.jsx";
 import Content from "./containers/Content.jsx";
 
 function LandingPage() {
+  useEffect(() => {
+    document.body.className = "landingpage-body"; // Apply a unique class to <body>
+
+    // Optional: Cleanup function to remove class when component unmounts
+    return () => {
+      document.body.className = "";
+    };
+  }, []);
+
   return (
-    <div>
-      <NavBar />
-      <Content />
-    </div>
+    <body className="landingpage-body">
+      <div>
+        <NavBar />
+        <Content />
+      </div>
+    </body>
   );
 }
 
